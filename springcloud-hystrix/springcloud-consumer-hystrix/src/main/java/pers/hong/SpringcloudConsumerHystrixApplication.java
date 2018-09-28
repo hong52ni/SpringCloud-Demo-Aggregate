d995a7754872352fc49fabdd53645bb87df46573
hong52ni <805258462@qq.com>
2018-09-28 20:49:58 +0800

    熔断器Hystrix配置

--- springcloud-hystrix/springcloud-consumer-hystrix/src/main/java/pers/hong/SpringcloudConsumerHystrixApplication.java
+++ springcloud-hystrix/springcloud-consumer-hystrix/src/main/java/pers/hong/SpringcloudConsumerHystrixApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudHystrixApplication {
+public class SpringcloudConsumerHystrixApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudHystrixApplication.class, args);
+        SpringApplication.run(SpringcloudConsumerHystrixApplication.class, args);
     }
 }
--- springcloud-hystrix/springcloud-consumer-hystrix/src/test/java/pers/hong/SpringcloudConsumerHystrixApplicationTests.java
+++ springcloud-hystrix/springcloud-consumer-hystrix/src/test/java/pers/hong/SpringcloudConsumerHystrixApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringcloudHystrixApplicationTests {
+public class SpringcloudConsumerHystrixApplicationTests {
 
     @Test
     public void contextLoads() {
