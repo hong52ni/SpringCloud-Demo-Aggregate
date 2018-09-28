194afc32bc3bc983511395289cf4e3078d8dbe3e
hong52ni <805258462@qq.com>
2018-09-28 11:20:50 +0800

    通过Feign进行服务调用

---
+++ springcloud-eureka/eureka-consumer-feign/src/main/java/pers/hong/EurekaConsumerFeignApplication.java
@@ -0,0 +1,16 @@
+package pers.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+public class EurekaConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerFeignApplication.class, args);
+    }
+}
